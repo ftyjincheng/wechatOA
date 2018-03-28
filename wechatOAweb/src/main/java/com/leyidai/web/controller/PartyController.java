a548291d7c7fcae735c6e7b41a265567eaee4b32
ftyjincheng <ftyjincheng@163.com>
2018-03-28 08:21:59 +0800

    2018.03.28

---
+++ wechatOAweb/src/main/java/com/leyidai/web/controller/PartyController.java
@@ -0,0 +1,12 @@
+package com.leyidai.web.controller;
+
+import org.springframework.stereotype.Controller;
+
+/**
+ * create by ftyjincheng
+ * date 2018/3/28
+ * 享受Coding时光
+ **/
+@Controller
+public class PartyController {
+}
